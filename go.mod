62678516b443213890176dbce116de4510127408
Tri Nguyen <minhtri.ngt@gmail.com>
2024-02-21 00:31:17 +0700

    define the lexer

---
+++ go.mod
@@ -0,0 +1,3 @@
+module minhtri6179/interpreter-in-go
+
+go 1.21.4
---
+++ token/token.go
@@ -0,0 +1,27 @@
+package token
+
+type TokenType string
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT     = "IDENT"
+	INT       = "INT"
+	ASSIGN    = "="
+	PLUS      = "+"
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
